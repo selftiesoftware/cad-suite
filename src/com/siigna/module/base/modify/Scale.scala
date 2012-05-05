1631788c4b1a39fcdb055e9155da0d9b4bb48123
Ole Egholm <oleegholm@gmail.com>
2012-05-05 17:03:42 +0200

    added text input to scale 2D

--- src/com/siigna/module/base/modify/Scale.scala
+++ src/com/siigna/module/base/modify/Scale.scala
@@ -17,24 +17,20 @@ import module.base.create.{PointGuides, PointGuide, AngleSnap}
 
 object Scale extends Module {
 
+  //TODO: scale the object FROM around the StartPoint not CENTERED around the StartPoint
+  //VARIABLES:
   var ending : Boolean = false
-
   var endPoint : Option[Vector2D] = None
-
   var gotEndPoint : Boolean = false
-
   var scaleFactor : Option[Vector2D] = None
-
   //a guide to get Point to draw the shape(s) dynamically
   val shapeGuide : Vector2D => Traversable[Shape] = (v : Vector2D) => {
 
     val refScale : Vector2D = startPoint.get - endPoint.get
-
     var scaleFactor = ((v - startPoint.get).length/refScale.length).toDouble
-
     // Create a matrix
     val t : TransformationMatrix = if (startPoint.isDefined) {
-      TransformationMatrix(Vector2D(0,0), 1).scale(scaleFactor)
+      TransformationMatrix(startPoint.get, 1).scale(scaleFactor)
     // If no startPoint has been defined - create an empty matrix
     } else TransformationMatrix()
     // Return the shape, transformed
@@ -42,9 +38,8 @@ object Scale extends Module {
   }
 
   var startPoint : Option[Vector2D] = None
-
+  var text = ""
   var transformation : Option[TransformationMatrix] = None
-
   def eventHandler = EventHandler(stateMap, stateMachine)
 
   def stateMap     = DirectedGraph(
@@ -76,7 +71,8 @@ object Scale extends Module {
       events match {
         case Message(p : Vector2D) :: tail => {
           endPoint = Some(p)
-          Goto('Scale)
+          Siigna display "type or click to set scale factor"
+          Goto('TextInput)
         }
         case MouseUp(p, _, _) :: MouseDown(_ ,_ ,_) :: tail => {
           ForwardTo('Point)
@@ -85,9 +81,36 @@ object Scale extends Module {
         case _ =>
       }
     }),
-    'Scale -> ((events : List[Event]) => {
-      Siigna display "set scale factor"
+    'TextInput -> ((events : List[Event]) => {
+      events match {
+        case MouseMove (p, _, _) :: tail => Goto('Scale)
+        case KeyDown(Key.Backspace, _) :: tail => {
+           if (text.length != 0) text = text.substring(0, text.length - 1)
+           else Goto('End)
+        }
+        case KeyDown(Key.Enter, _) :: tail => {
+          var numbers = text.toDouble
+          transformation = Some(TransformationMatrix(startPoint.get,1))
 
+          //perform scaling:
+          Model.selection.get.transform(transformation.get.scale(numbers))
+
+          text = ""
+
+          Goto('End)
+        }
+        case KeyDown(Key.Esc, _) :: tail => {
+          text = ""
+          Goto('End)
+        }
+        case KeyDown(key, _) :: tail => {
+          text += key.toChar.toString.toLowerCase
+          Siigna display text
+        }
+        case _ => None
+      }
+    }),
+    'Scale -> ((events : List[Event]) => {
       //check if the endPoint is set. If not, goto 'Point.
       if (gotEndPoint == false) {
         gotEndPoint = true
@@ -105,10 +128,9 @@ object Scale extends Module {
             //})
 
             val refScale : Vector2D = startPoint.get - endPoint.get
-
             var scaleFactor = ((p - startPoint.get).length/refScale.length).toDouble
 
-            transformation = Some(TransformationMatrix(Vector2D(0,0),1))
+            transformation = Some(TransformationMatrix(startPoint.get,1))
             Model.selection.get.transform(transformation.get.scale(scaleFactor))
             //Model.selection.get.shapes.foreach(tuple => {
             //  UpdateShape(AppletParameters.getDrawingId.get, tuple._1, oldShapes(tuple._1), tuple._2, AppletParameters.getClient)
