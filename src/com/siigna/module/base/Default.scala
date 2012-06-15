c25111cdb41f047fb4ea8c5d47e082fc035cd348
Ole Egholm <oleegholm@gmail.com>
2012-06-15 23:26:42 +0200

    incremented version number to 0.8, removed println

--- src/com/siigna/module/base/Default.scala
+++ src/com/siigna/module/base/Default.scala
@@ -86,7 +86,7 @@ object Default extends Module {
         //start the loading bar timer
         startTime =  Some(System.currentTimeMillis().toLong)
 
-        Siigna.display("Loading Siigna modules ver. 0.7")
+        Siigna.display("Loading Siigna modules ver. 0.8")
         //preload commonly used modules
         Preload('AngleGizmo, "com.siigna.module.base.create")
         Preload('Area, "com.siigna.module.base.helpers")
@@ -153,7 +153,6 @@ object Default extends Module {
           key.toChar match {
             case Key.Backspace | Key.Delete => {
               if (Model.selection.isDefined) {
-                println("Deleting selection: " + Model.selection)
                 Delete(Model.selection.get)
               }
             }
