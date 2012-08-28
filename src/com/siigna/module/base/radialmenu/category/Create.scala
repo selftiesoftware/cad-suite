cdb297a1f1c3d036d61e87d7eb330e7c3ab46792
Oleegholm <oleegholm@gmail.com>
2012-08-28 13:39:27 +0200

    fixed a bug in track

--- src/com/siigna/module/base/radialmenu/category/Create.scala
+++ src/com/siigna/module/base/radialmenu/category/Create.scala
@@ -23,24 +23,32 @@ case class Create(parent : Option[MenuCategory]) extends MenuCategory {
 
   def name = "Create"
 
-  //override def E = Some(MenuItemEmpty("Create from"))
+  //CATEGORIES:
+  override def E = Some(MenuItemEmpty("Create from"))
   override def N = Some(new Geometry(Some(this)))
   override def W = Some(CategoryItemEmpty("Annotation"))
   override def S = Some(CategoryItemEmpty("Collection"))
   //override def S = Some(new Dimension(Some(this)))
-  override def E = Some(CategoryItemEmpty("Script"))
+  //override def E = Some(CategoryItemEmpty("Script"))
 
+  //TOOLS:
+
+  //NORTH
   override def NNW = Some(MenuItem('Rectangle, RadialMenuIcon.rectangle))
   override def NNE = Some(MenuItem('Line, RadialMenuIcon.line , "create"))
 
-  override def WNW = Some(MenuItem('Text, RadialMenuIcon.text))
-  //override def NNW = Some(MenuItem('Fill, RadialMenuIcon.raster, "create"))
+  //SOUTH
   //override def SSW = Some(MenuItem('Polyline, RadialMenuIcon.group))
   override def SSE = Some(MenuItem('Explode, RadialMenuIcon.explode, "create"))
-  override def WSW = Some(MenuItem('Lineardim, RadialMenuIcon.linearDimension))
-  //override def ESE = Some(MenuItem('Offset, RadialMenuIcon.offset, "create"))
 
-  override def ESE = Some(MenuItem('ScriptEditor, TextShape("editor", Vector2D(0, 0), 7, Attributes("TextAlignment" -> Vector2D(0.5, 0.5))), "create"))
-  override def ENE = Some(MenuItem('ScriptExecution, TextShape("run", Vector2D(0, 0), 7, Attributes("TextAlignment" -> Vector2D(0.5, 0.5))), "create"))
+  //WEST
+  override def WNW = Some(MenuItem('Text, RadialMenuIcon.text))
+  //override def NNW = Some(MenuItem('Fill, RadialMenuIcon.raster, "create"))
+  override def WSW = Some(MenuItem('Lineardim, RadialMenuIcon.linearDimension))
+
+  //EAST
+  override def ESE = Some(MenuItem('Offset, RadialMenuIcon.offset, "create"))
+  //override def ESE = Some(MenuItem('ScriptEditor, TextShape("editor", Vector2D(0, 0), 7, Attributes("TextAlignment" -> Vector2D(0.5, 0.5))), "create"))
+  //override def ENE = Some(MenuItem('ScriptExecution, TextShape("run", Vector2D(0, 0), 7, Attributes("TextAlignment" -> Vector2D(0.5, 0.5))), "create"))
 
 }
