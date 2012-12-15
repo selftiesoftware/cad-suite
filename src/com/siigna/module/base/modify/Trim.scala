e40333945b5c25e0ffad4360a51efd795d62af33
Oleegholm <oleegholm@gmail.com>
2012-12-15 16:18:31 +0100

    basic implementation of trim (lineShapes only)

--- src/com/siigna/module/base/modify/Trim.scala
+++ src/com/siigna/module/base/modify/Trim.scala
@@ -28,9 +28,30 @@ class Trim extends Module {
   var selectionBoxStart : Option[Vector2D] = None
   var selectionBoxEnd : Option[Vector2D] = None
 
+  //calculate on which side of a given line segment the trim should be.
+  def trimSide (l: Line2D, m : Vector2D) : Boolean = {
+    val s = LineShape(l.p1,l.p2)
+    val angleRaw = ((s.p2-s.p1).angle * -1) + 450
+    //reformat the angles to lie between 0 and 180
+    val angle1 = if(angleRaw > 360) angleRaw - 360 else angleRaw
+    val angle = if(angle1 > 180) angle1 - 180 else angle1 //
+    val linePt = s.geometry.closestPoint(m)//get a point on the line to evaluate against
+    val ly = linePt.y
+    val my = m.y
+    //if the mouse is north of the shape, offset should extend upwards
+    if(angle > 0 && angle < 90) {
+      if(my > ly)true
+      else false
+    }
+    //if the mouse is south of the shape, offset should extend downwards
+    else {
+      if(my < ly) false
+      else true
+    }
+  }
+  
   //evaluates a guideline and a (poly)line returns the polyline, trimmed by the line.
   def trim(guide : Geometry2D, trimLine : Geometry2D, p : Vector2D) = {
-    println("A")
     val g = guide.vertices
     val t = trimLine.vertices
     var trimV = List[Vector2D]()
@@ -39,12 +60,22 @@ class Trim extends Module {
     for (i <- 0 to t.length -2) {
       val l1 = Line2D(t(i),t(i+1)) //create a line segment to evaluate
       //TODO: allow trimming of lines that intersect the guide multiple times
-      //if the guide and the segment intersects, get the intersection.
       //TODO: add a way to exclude intersections that happen in the extension of the segment
-      val int = l1.intersections(gLine).head
-      trimV = trimV :+ t(i) //add the first point on the segment - unalteres
-      //TODO: add a mechanism to evaluate which side of the line sebment should be trimmed
-      trimV = trimV :+ int  //add the new intersection point instead of the second point
+      //val isIntersecting = l1.intersects(guide)
+      //println("inr ?: "+isIntersecting)
+      //if the guide and the segment intersects, get the intersection.
+      val int = l1.intersections(gLine)
+      //test on what side of the intersection the trimming should take place
+      val getTrim = {
+        if(!int.isEmpty && trimSide(gLine, p) == false) {
+          trimV = trimV :+ t(i) //add the first point on the segment - unaltered
+          trimV = trimV :+ int.head  //add the new intersection point instead of the second point
+        } else if (!int.isEmpty && trimSide (gLine, p) == true){
+          trimV = trimV :+ t(i+1) //add the first point on the segment - unaltered
+          trimV = trimV :+ int.head  //add the new intersection point instead of the second point
+        }
+      }
+      getTrim
     }
     trimV
   }
