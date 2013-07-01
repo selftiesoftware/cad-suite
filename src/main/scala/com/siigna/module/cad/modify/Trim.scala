c7bb28582b5349223217bb125dba500d5824a179
oleegholm <oleegholm@gmail.com>
2013-07-01 12:46:00 +0200

    started reimplementing trim with test

--- src/main/scala/com/siigna/module/cad/modify/Trim.scala
+++ src/main/scala/com/siigna/module/cad/modify/Trim.scala
@@ -27,19 +27,49 @@ class Trim extends Module {
 
   /*
    * a function to trim a polylineShape
-   * s1 = the trimGuideShape
-   * s2 = the shape to be trimmed
+   * gs = the trimGuideShapes
+   * ts = the shape to be trimmed
+   * p = trim point (the part of s2 which should be deleted
+   *
+   *  SITUATION A - one trimPoint
+   *
+   *            tp
+   *            |            s2
+   *      *-----|------ * p ----------*
+   *            |
+   *
+   *  SITUATION B - two or more trimPoints, p between two of them
+   *
+   *            tp        tp
+   *            |          |   ts
+   *      *-----|--- * p --|------*
+   *            |          |
+   *
+   * SITUATION C - two or more trimPoints, p not between two of them
+   *
+   *            tp        tp
+   *            |          |        ts
+   *      *-----|----------|-- * p ----*
+   *            |          |
    */
 
-  def trimTwoShapes(s1 : Shape, s2 : Shape) = {
-    if (s1.geometry.intersects(s2.geometry)) {
-      val ints = s1.geometry.intersections(s2.geometry)
-      println("s1 geom: "+s1.geometry)
-      println("s2 geom: "+s2.geometry)
-      println("ints: "+ints)
-      ints
+  def trimTwoPolyLineShapes(gs : List[PolylineShape], ts : PolylineShape, p : Vector2D) = {
 
-    } else println("NO INTERSECTION")
+    var ints = List[Set[Vector2D]]()
+
+    gs.foreach(g => {
+      //get the intersections
+      if (g.geometry.intersects(ts.geometry)) ints = g.geometry.intersections(ts.geometry) :: ints else println("NO INTERSECTION")
+
+      if(ints.length == 1) {
+        println("one trimPoint")
+      }
+      else if (ints.length > 1) {
+        println("more than one trimPoints")
+      }
+      else None
+
+    })
   }
 
   val trimGuideShapes : List[Shape] = List()
--- src/test/scala/com/siigna/module/cad/modify/TrimSpec.scala
+++ src/test/scala/com/siigna/module/cad/modify/TrimSpec.scala
@@ -22,16 +22,31 @@ import com.siigna.app.model.shape.PolylineShape
 
 class TrimSpec extends FunSpec with ShouldMatchers {
 
-  val lineVert = PolylineShape(List(Vector2D(-100,0),Vector2D(10,10), Vector2D(100,0)))
-  val lineHoriz1 = PolylineShape(List(Vector2D(-10,20),Vector2D(-15,-30)))
-  val lineHoriz2 = PolylineShape(List(Vector2D(10,20),Vector2D(10,-30)))
+  val trimLine = PolylineShape(List(Vector2D(-100,0),Vector2D(10,10), Vector2D(100,0)))
+  val gs1 = PolylineShape(List(Vector2D(-10,20),Vector2D(-15,-30)))
+  val gs2 = PolylineShape(List(Vector2D(10,20),Vector2D(10,-30)))
+
+  val oneGuide = List(gs1)
+  val twoGuides = List(gs1, gs2)
+
+  val p1 = Vector2D(0,9.09175)
+  val p2 = Vector2D(-50,4.54321)
 
   //instantiate the trim class
   val trimModule = new Trim
 
-  describe("polylineShapes... ") {
-    it("can be trimmed by another polyline") {
-      trimModule.trimTwoShapes(lineVert,lineHoriz1) should equal(Set(Vector2D(-11.192660550458712,8.073394495412845)))
+  describe("a PolylineShape... ") {
+    it("can be trimmed by another PolylineShape") {
+
+      trimModule.trimTwoPolyLineShapes(oneGuide,trimLine,p1) should equal(Set(PolylineShape(Vector2D(-100,0),Vector2D(-11.192660550458712,8.073394495412845))))
     }
+
+    //it("can be trimmed by two PolylineShapes when the trim point is between the two PolylineShapes") {
+
+    //  trimModule.trimTwoPolyLineShapes(twoGuides,trimLine,p1) should equal(Set(PolylineShape(Vector2D(-100,0),Vector2D(-11.192660550458712,8.073394495412845)),PolylineShape(Vector2D(10,10),Vector2D(100,0))))
+      //p not between the lines
+    //  trimModule.trimTwoPolyLineShapes(twoGuides,trimLine,p2) should equal(Set(PolylineShape(Vector2D(-100,0),Vector2D(-11.192660550458712,8.073394495412845))))
+    //}
   }
+
 }
