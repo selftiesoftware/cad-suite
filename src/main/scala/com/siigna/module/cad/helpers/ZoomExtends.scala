1f0d3a40e686db1d58841c3a0c3648af30e3c0cc
Jegp <jensep@gmail.com>
2013-07-29 16:03:40 +0200

    Fixed zoomExtends Added possibility to set module stability in SiignaAttributes and SiignaApplet Commented out line to take siigna offline

--- src/main/scala/com/siigna/module/cad/helpers/ZoomExtends.scala
+++ src/main/scala/com/siigna/module/cad/helpers/ZoomExtends.scala
@@ -1,15 +1,14 @@
 package com.siigna.module.cad.helpers
 
 import com.siigna._
-import com.siigna.module.Module
 
-class ZoomExtends extends Module{
+class ZoomExtends extends Module {
 
   val stateMap : StateMap = Map(
 
     'Start-> {
       case _ => {
-        View.zoomExtends()
+        View.zoomExtends
         End
       }
     }
