b0da441042c82ca29d23673a3b5079254b663fc2
Niels <nielsegholm@gmail.com>
2013-12-21 21:31:06 +0100

    Updated help text

--- src/main/scala/com/siigna/module/ModuleInit.scala
+++ src/main/scala/com/siigna/module/ModuleInit.scala
@@ -252,10 +252,13 @@ class ModuleInit extends Module {
       case End :: tail => if(Tooltip.lastUpdate +500 > System.currentTimeMillis()) Tooltip.blockUpdate(3500)
 
       case y => {
-        //revert to the arrow-type cursor
 
+        //revert to the arrow-type cursor
         Siigna.setCursor(defaultCursor)
         Tooltip.updateTooltip(List("Click right key to access drawing tools","Keyboard shortcuts: C = Create, H = Helpers, E = Edit, F = File.","SPACE = last tool, ALT = pan."))
+        if (Drawing.size == 0) {
+          Siigna tooltip(List("Right click to open menu","Here you'll find the drawing tools", "Suggestions for improvements? Please use the comment box above"))
+        }
         Start('cad, "create.Input", InputRequest(14, None))
       }
     }
